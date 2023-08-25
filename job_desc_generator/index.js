621a3b29ec09db06bf624c032f90ebed476f80df
lumi808 <108948664+lumi808@users.noreply.github.com>
2023-08-25 17:47:25 +0600

    fix: optimized code

--- job_desc_generator/index.js
+++ job_desc_generator/index.js
@@ -14,38 +14,38 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_KEY});
 app.post('/generate-from-task', async (req, res)=>{
     const { task } = req.body;
 
-    const messages = [
-        { role: "user", content: task },
-        {
-            role: "system",
-            content: `You are a job description writer assistant. You should write job description and requirements according to tasks that user will provide to you. Here is the example how job description should look like and formatted: 
-            "Description: 
-            *job description*
-            Responsibilities:
-            *responsibilities*
-            Requirements:
-            *requirements*
-            "
-            everything should be written with bullet points
-            `
-        }
-    ];
-
-    const payload = {
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        temperature: 0,
-        max_tokens: 1024,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        n: 1,
-    };
-
     try{
-        if(task === null){
+        if(task === null || task === ''){
             return res.status(400).send('Missing prompt');
         }
+
+        const messages = [
+            { role: "user", content: task },
+            {
+                role: "system",
+                content: `You are a job description writer assistant. You should write job description and requirements according to tasks that user will provide to you. Here is the example how job description should look like and formatted: 
+                "Description: 
+                *job description*
+                Responsibilities:
+                *responsibilities*
+                Requirements:
+                *requirements*
+                "
+                everything should be written with bullet points
+                `
+            }
+        ];
+    
+        const payload = {
+            model: 'gpt-3.5-turbo',
+            messages: messages,
+            temperature: 0,
+            max_tokens: 1024,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            n: 1,
+        };
 
         const response = await openai.chat.completions.create(payload);
         const jobDescription = response.choices[0].message.content;
