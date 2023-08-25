616569b4c04d6f5e9f55049a4aa812a0609206d2
lumi808 <108948664+lumi808@users.noreply.github.com>
2023-08-26 00:22:09 +0600

    feat: added generation of description from project route

--- job_desc_generator/index.js
+++ job_desc_generator/index.js
@@ -57,8 +57,50 @@ app.post('/generate-from-task', async (req, res)=>{
     }
 });
 
-app.post('/generate-from-project', (req, res)=>{
-    const { project } = req;
+app.post('/generate-from-project', async (req, res)=>{
+    const { project } = req.body;
+
+    try{
+        if(project === null || project === ''){
+            return res.status(400).send('Missing prompt');
+        }
+
+        const messages = [
+            { role: "user", content: project },
+            {
+                role: "system",
+                content: `You are a job description writer assistant. You should write job description and requirements according to project description and role that user will provide to you. Here is the example how job description should look like and formatted: 
+                "Description: 
+                *job description*
+                Responsibilities:
+                *responsibilities*
+                Requirements:
+                *requirements*
+                "
+                everything should be written with bullet points                
+                `
+            }
+        ];
+    
+        const payload = {
+            model: 'gpt-3.5-turbo',
+            messages: messages,
+            temperature: 0,
+            max_tokens: 1024,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            n: 1,
+        };
+
+        const response = await openai.chat.completions.create(payload);
+        const jobDescription = response.choices[0].message.content;
+
+        return res.status(200).send(jobDescription);
+    } catch(error){
+        console.error(error);
+        res.status(500).send('Error while generating prompt');
+    }
 }); 
 
 app.listen(port, ()=>{
