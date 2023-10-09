b5fcc0b41fbaf4ccd81f9ba4fb80c37b973c9d3b
lumi808 <108948664+lumi808@users.noreply.github.com>
2023-10-09 23:43:27 +0600

    cors

--- search_canidate/index.js
+++ search_canidate/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:3000', 'https://agile-job-front.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://agile-job-front.vercel.app', 'https://testapp.ediploma.kz'];
 
 app.use(cors({
     origin: (origin, callback)=>{
